cabefce6320a86d6ba3a0fe1f1d0b1482af2a5c2
Anton Olsson <antonolsson.imp@gmail.com>
2023-09-05 13:12:00 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Nasa Astronomy Picture of the Day
+
+### Use case
+
+This script is used for fetching the Astronomy picture of the day using the Nasa API.
+When the image is saved in a buffer i then use the Dropbox API to save the image in a "Nasa"-folder in my personal dropbox. Just to have them close by without googling it every day 😁🔧⚙️
+
+I have scheduled this script to run once a day on my Raspberry Pi 3B to kind of automating it!d
---
+++ index.js
@@ -0,0 +1,44 @@
+require("dotenv").config()
+
+const axios = require("axios")
+const Dropbox = require("dropbox")
+
+const apiKey = process.env.API_KEY
+
+const accessToken = process.env.ACCESS_TOKEN
+
+const dbx = new Dropbox.Dropbox({ accessToken })
+
+const getData = () => {
+    axios
+        .get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
+        .then((res) => {
+            const hdURL = res.data.hdurl
+            const title = res.data.title
+            const dropboxPath = `/${encodeURIComponent(title)}.jpg`
+
+            axios({
+                method: "get",
+                url: hdURL,
+                responseType: "arraybuffer",
+            }).then((imgRes) => {
+                dbx.filesUpload({
+                    path: dropboxPath,
+                    contents: imgRes.data,
+                })
+                    .then(() => {
+                        console.log(
+                            `Image was successfully uploaded to Dropbox: ${title}.jpg`
+                        )
+                    })
+                    .catch((uploadErr) => {
+                        console.error(
+                            `Error uploading image to Dropbox. Status code: ${uploadErr.status}; Error message: ${uploadErr}`
+                        )
+                    })
+            })
+        })
+        .catch((err) => console.log(err))
+}
+
+getData()
