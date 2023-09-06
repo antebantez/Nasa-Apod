966278a45a02164f214f149556635f260f9d94b4
Anton Olsson <antonolsson.imp@gmail.com>
2023-09-06 11:33:27 +0200

    Media_type check
    In case of the content from the response isnt a image, then save the url to the content for example a video in a text file in the dropbox.

--- index.js
+++ index.js
@@ -13,30 +13,49 @@ const getData = () => {
     axios
         .get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
         .then((res) => {
-            const hdURL = res.data.hdurl
-            const title = res.data.title
-            const dropboxPath = `/${encodeURIComponent(title)}.jpg`
+            if (res.media_type === "image") {
+                console.log(res.data)
+                const hdURL = res.data.hdurl
+                const title = res.data.title
+                const dropboxPath = `/${encodeURIComponent(title)}.jpg`
 
-            axios({
-                method: "get",
-                url: hdURL,
-                responseType: "arraybuffer",
-            }).then((imgRes) => {
+                axios({
+                    method: "get",
+                    url: hdURL,
+                    responseType: "arraybuffer",
+                }).then((imgRes) => {
+                    dbx.filesUpload({
+                        path: dropboxPath,
+                        contents: imgRes.data,
+                    })
+                        .then(() => {
+                            console.log(
+                                `Image was successfully uploaded to Dropbox: ${title}.jpg`
+                            )
+                        })
+                        .catch((uploadErr) => {
+                            console.error(
+                                `Error uploading image to Dropbox. Status code: ${uploadErr.status}; Error message: ${uploadErr}`
+                            )
+                        })
+                })
+            } else {
+                const { title, url } = res.data
                 dbx.filesUpload({
-                    path: dropboxPath,
-                    contents: imgRes.data,
+                    path: `/${encodeURIComponent(title)}.txt`,
+                    contents: Buffer.from(url, "utf-8"),
                 })
                     .then(() => {
                         console.log(
-                            `Image was successfully uploaded to Dropbox: ${title}.jpg`
+                            `File was successfully uploaded to Dropbox: ${title}.txt`
                         )
                     })
                     .catch((uploadErr) => {
                         console.error(
-                            `Error uploading image to Dropbox. Status code: ${uploadErr.status}; Error message: ${uploadErr}`
+                            `Error uploading file to Dropbox. Status code: ${uploadErr.status}; Error message: ${uploadErr}`
                         )
                     })
-            })
+            }
         })
         .catch((err) => console.log(err))
 }
